8ac4008b3e7204d5dd844d3d3b92017cf4f1c00f
stann-blogg <stanleydzombo10@gmail.com>
2026-07-05 21:58:27 +0300

    Add JavaScript string methods practice

--- stann.js
+++ stann.js
@@ -1,2 +1,25 @@
-let name = "STAN";
-console.log("HEY " + name);
+// 
+
+console.log("good morning stanley")
+let email = "stanleydzombo10@gmail.com"
+console.log(email);
+
+let firstName = "Stanley"
+let lastName = "Dzombo"
+let fullName = firstName + " " + lastName
+console.log(fullName);
+console.log(fullName [0]);
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+
+// arguments
+console.log(fullName.indexOf("D")); 
+
+// common string methods
+let index = email.lastIndexOf("a");
+console.log(index);
