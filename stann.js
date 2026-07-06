b890035f581202f248874997a0f1607d5bd883a0
stann-blogg <stanleydzombo10@gmail.com>
2026-07-06 15:59:24 +0300

    math operators

--- stann.js
+++ stann.js
@@ -1,25 +1,64 @@
+// // 
+// strings
+
+// console.log("good morning stanley")
+// let email = "stanleydzombo10@gmail.com"
+// console.log(email);
+
+// let firstName = "Stanley"
+// let lastName = "Dzombo"
+// let fullName = firstName + " " + lastName
+// console.log(fullName);
+// console.log(fullName [0]);
+// console.log(fullName.length);
+
+// // string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result);
+
+
+// // arguments
+// console.log(fullName.indexOf("D")); 
+
+// // common string methods
+// let index = email.lastIndexOf("a");
+// console.log(index);
+
+// let email = "stanleydz0mb010@gmail.com";
+// console.log(email.slice(0, 15));
+
+// making a sybstring
+
+// let email = "stanleydz0mb010@gmail.com";
+// let result = email.replace("stanleydz0mb010", "stanley");
+// console.log(result);
+
+// let email = "stanleydz0mb010@gmail.com";
+// let result = email.substring(5, 10);
+// console.log(result);
+
+// numbers
+let radius = 10;
+const pi = 3.14;
 // 
+// math operators +, -, *, /, **, %
 
-console.log("good morning stanley")
-let email = "stanleydzombo10@gmail.com"
-console.log(email);
+// 
+// 
+let result = pi * radius**2;
+console.log(result); 
 
-let firstName = "Stanley"
-let lastName = "Dzombo"
-let fullName = firstName + " " + lastName
-console.log(fullName);
-console.log(fullName [0]);
-console.log(fullName.length);
+// order of operations - B I D M A S
 
-// string methods
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
-console.log(result);
+let result2 = 5 * (10 - 3)**2;
+console.log(result2);
 
+// let likes = 10;
+// likes++;
+// console.log(likes);
 
-// arguments
-console.log(fullName.indexOf("D")); 
+let likes = 10;
+likes += 10;
+console.log(likes);
 
-// common string methods
-let index = email.lastIndexOf("a");
-console.log(index);
